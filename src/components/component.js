671e76b94c283b88db7811f5b766b42bbff8517c
moldovaniulia <62939385+moldovaniulia@users.noreply.github.com>
2020-07-17 14:49:10 +0300

    Commit made using teleportHQ

--- src/components/component.js
+++ src/components/component.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import styles from './component.module.css'
 
 const AppComponent = (props) => {
-  return <div className={styles.container}></div>
+  return (
+    <div className={styles.container}>
+      <img src={props.src} className={styles.image} />
+    </div>
+  )
+}
+
+AppComponent.defaultProps = {
+  src: '/playground_assets/png-1000w.png',
+}
+
+AppComponent.propTypes = {
+  src: PropTypes.string,
 }
 
 export default AppComponent
--- src/views/page.js
+++ src/views/page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
+import AppComponent from '../components/component'
 import styles from './page.module.css'
 
 const Page = (props) => {
@@ -11,6 +12,7 @@ const Page = (props) => {
         <meta property="og:title" content="poc" />
       </Helmet>
       <h1>Heading</h1>
+      <AppComponent src="/playground_assets/mae-mu-f_cNyVCExTM-unsplash-1200h.jpg"></AppComponent>
     </div>
   )
 }
